780c816052d1c27b4afccff3caebdcee13ffca24
Thomas Hanson <thomas@diresquirrel.com>
2014-03-02 21:40:39 -0800

    Install devise for user authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025  }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   RequireReloader.watch_local_gems!
 end
